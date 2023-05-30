0f69caef30ab8d9a65f71f380b2ab3baf1cd2de3
Volkan Kücükbudak <VolkanSah@users.noreply.github.com>
2023-05-30 18:02:52 +0200

    Update feuerteufel.py

--- feuerteufel.py
+++ feuerteufel.py
@@ -3,31 +3,24 @@ from stem import Signal
 from stem.control import Controller
 import requests
 import re
-
 # Get onion address from monitoring script loaded before
 onion_address = "INSERT .ONION ADDRESS HERE"  # .onion address you want to monitor
-
 # Set controller
 with Controller.from_port(port=9051) as controller:
     controller.authenticate()
-
 # Set Stream Listener
 class MaliciousTrafficListener(StreamListener):
     def __init__(self, keywords):
         self.keywords = keywords
-
     def stream_new(self, stream):
         if any(re.search(keyword, stream.target_host) for keyword in self.keywords):
             print(f"Malicious traffic detected to {stream.target_host}")
-
 # Register the Stream Listener
 with Controller.from_port(port=9051) as controller:
     controller.authenticate()
     listener = MaliciousTrafficListener(["abuse", "child"])
     controller.add_event_listener(listener)
-
     input("Press Enter to exit")
-
 def check_if_tor_traffic(packet):
     """
     Check if the packet belongs to Tor traffic.
@@ -44,7 +37,6 @@ def check_if_tor_traffic(packet):
             if controller.get_info("address") == packet[IP].dst:
                 return True
     return False
-
 def intercept_api_requests(request):
     """
     Intercept API requests and execute an external script.
@@ -54,14 +46,14 @@ def intercept_api_requests(request):
     """
     if sniff_packets():
         # This is where you would load and execute your external script.
-        # Remember that executing code fetched from the internet can be risky.
+        # Remember that executing code fetched from the internet can be risky 
+        # e.g external_script_url = "/opt/your_folder/external_script.py"
         external_script_url = "https://example.com/external_script.py"
         response = requests.get(external_script_url)
         if response.status_code == 200:
             exec(response.text)
         else:
             print("Failed to load the external script.")
-
 def sniff_packets():
     """
     Sniff packets and filter for Tor traffic.
@@ -72,7 +64,6 @@ def sniff_packets():
     packets = sniff(filter="tcp and (port 9050 or port 9051)", prn=check_if_tor_traffic)
 
     return packets
-
 def get_circuit_hops():
     """
     Get the current circuit and its hops.
@@ -84,9 +75,7 @@ def get_circuit_hops():
         controller.authenticate()
         circuit_id = controller.get_circuit_id()
         hops = controller.get_circuit(circuit_id).path
-
     return hops
-
 def exclude_malicious_relays():
     """
     Add malicious relays to the circuit blacklist.
